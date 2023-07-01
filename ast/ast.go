afc08694ac59369c88fbcc895899d0780b8026f5
cazser <2264712072@qq.com>
2023-07-01 14:22:54 +0800

    add letstatement

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,5 @@
 package ast
-
+import "monkey/token"
 type Node interface{
 	TokenLiteral() string
 }
@@ -27,3 +27,24 @@ func (p *Program) TokenLiteral() string{
 		return "";
 	}
 }
+
+
+type LetStatement struct{
+	Token token.Token 
+	Name *Identifier
+	Value Expression 
+}
+
+
+func (ls *LetStatement) statementNode(){}
+func (ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
+
+type Identifier struct{
+	Token token.Token 
+	Value string 
+}
+
+
+func (i *Identifier) expressionNode(){}
+
+func (i *Identifier) TokenLiteral() string{ return i.Token.Literal}

ae7af35a2989778fd19f58da824ca08287676e63
cazser <2264712072@qq.com>
2023-07-01 15:35:19 +0800

    +1

--- parser/parser.go
+++ parser/parser.go
@@ -42,3 +42,13 @@ func (p *Parser) ParseProgram() *ast.Program{
 	return program;
 }
 
+
+func (p *Parser) parseStatement() ast.Statement{
+	switch p.curToken.Token{
+	case token.LET:
+		return p.parseLetStatement();
+	default: 
+	  return nil;
+	}
+}
+
